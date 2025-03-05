133d343ac01564eef0fee6d211d38b44c9e1d1cf
m-najeeb <muhammadnajeeb2022@gmail.com>
2025-03-05 15:51:21 +0500

    classes in TS

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,40 @@
 "use strict";
-console.log("TS file here");
+// class User {
+//   public email: string;
+//   private name: string;
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+class User {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this._courseCount = 1;
+    }
+    get getEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        else {
+            this._courseCount = courseNum;
+        }
+    }
+}
+class SubUser extends User {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+    changeCourseCount() {
+        this._courseCount = 4;
+    }
+}
+const john = new User("john@example.com", "john");
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-console.log("TS file here");
+// class User {
+//   public email: string;
+//   private name: string;
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+
+class User {
+  protected _courseCount = 1;
+  constructor(public email: string, public name: string) {}
+  get getEmail(): string {
+    return `apple${this.email}`;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  set courseCount(courseNum) {
+    if (courseNum <= 1) {
+      throw new Error("Course count should be more than 1");
+    } else {
+      this._courseCount = courseNum;
+    }
+  }
+}
+
+class SubUser extends User {
+  isFamily: boolean = true;
+  changeCourseCount() {
+    this._courseCount = 4;
+  }
+}
+
+const john = new User("john@example.com", "john");
