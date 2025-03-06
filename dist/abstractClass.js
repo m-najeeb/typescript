f218db5c5f868724e821a410465cf1de21026505
m-najeeb <muhammadnajeeb2022@gmail.com>
2025-03-06 09:20:57 +0500

    commenting file for abstract class

---
+++ dist/abstractClass.js
@@ -0,0 +1,19 @@
+"use strict";
+class TakePhoto {
+    constructor(cameraMode, filter) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+    }
+}
+class Instagram extends TakePhoto {
+    constructor(cameraMode, filter, burst) {
+        super(cameraMode, filter);
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+    }
+    getSepia() {
+        console.log("Sepia");
+    }
+}
+const johnSr = new Instagram("test", "test", 3);
